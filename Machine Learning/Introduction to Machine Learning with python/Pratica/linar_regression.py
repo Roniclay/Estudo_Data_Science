12a15ea4459e8d293f56a5a00fd932d9782fa225
Roniclay Rodrigues <roniclayrodrigues07@gmail.com>
2023-11-03 10:53:03 -0300

    Atualizando modelos Lineares para classificação

--- Machine Learning/Introduction to Machine Learning with python/Pratica/linar_regression.py
+++ Machine Learning/Introduction to Machine Learning with python/Pratica/linar_regression.py
@@ -2,6 +2,8 @@ import mglearn.datasets
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+import numpy as np
 
 
 """X, y = mglearn.datasets.make_wave(n_samples=60)
@@ -21,7 +23,7 @@ lr = LinearRegression().fit(X_train, y_train)
 print(f"Training score: {lr.score(X_train, y_train)}")
 print(f"Test score: {lr.score(X_test, y_test)}")"""
 
-X,y = mglearn.datasets.load_extended_boston()
+"""X,y = mglearn.datasets.load_extended_boston()
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 ridge = Ridge().fit(X_train,y_train)
@@ -30,4 +32,13 @@ print(f"Training set score: {ridge.score(X_train, y_train)}")
 print(f"Test sset score: {ridge.score(X_test, y_test)}")
 print()
 print(f"Training set score: {ridge10.score(X_train, y_train)}")
-print(f"Test sset score: {ridge10.score(X_test, y_test)}")
+print(f"Test sset score: {ridge10.score(X_test, y_test)}")"""
+
+
+X,y = mglearn.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+lasso = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
+print(f"Training set score: {lasso.score(X_train, y_train)}")
+print(f"Test sset score: {lasso.score(X_test, y_test)}")
+print(f"Number of features used: {np.sum(lasso.coef_ != 0)}")
